8a59ea809dc23d2bdf29cfa873012087b39d0cc6
Evgenia <zhenyafka163@gmail.com>
2024-08-08 11:38:18 +0200

    created interfaces

---
+++ src/components/tables.tsx
@@ -0,0 +1,17 @@
+export interface Bearings {
+    id: number;
+    name: string;
+    count: number;
+    price: number;
+    brand: string;
+    inner_diameter: number;
+    outer_diameter: number;
+    height: number;
+    type: string;
+    country: string;
+}
+
+export interface Brands {
+    id: number;
+    name: string;
+}
